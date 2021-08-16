dc6c197203fbe7e54ae00fb4f0c3a75b4e1708ee
Antonio DroCode <antuansabe@gmail.com>
2021-08-16 12:54:11 -0500

    feat(product-component) lifecicle and ng directives

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,13 +1,44 @@
-import { Component, Input, Output, EventEmitter}  from '@angular/core';
+import { Component,
+    Input,
+    OnDestroy,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChange, 
+    SimpleChanges,
+    OnInit, 
+    DoCheck} 
+    from '@angular/core';
 import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
     templateUrl: '../components/product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('constructor');
+    }
+
+  //  ngOnChanges(changes: SimpleChanges) {
+  //      console.log('ngOnChanges');
+  //      console.log(changes);
+  //  }
+
+    ngOnInit () {
+        console.log('ngOnInit');
+    }
+
+    ngDoCheck(){
+        console.log('ngDoCheck');
+    }
+    
+    ngOnDestroy(){
+        console.log('ngOnDestroy');
+    }
 
     addCart(){
         console.log('añadir al carrito');
