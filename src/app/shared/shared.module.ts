c8cfb653019e341f18233c2a366e2e344344d707
Antonio DroCode <antuansabe@gmail.com>
2021-08-20 18:56:12 -0500

    fix(shared-module) importacion de rutas para arreglar el routing con los nuevos modulos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,7 +18,8 @@ import { FooterComponent } from './components/footer/footer.component';
     ExponentialPipe
     ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
